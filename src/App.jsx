d05ca3300485a28f994ef8aa7a8edf05caee4c5e
CletsyMedia <Cletussam12@yahoo.com>
2024-04-04 23:46:33 +0100

    Hackathon begins

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import Apifetch from "./Components/Apifetch";
+import ComponentA from "./Components/ComponentA";
+import PostForm from "./Components/PostForm";
+import Todo from "./Components/Todo";
+
+
+
+const App = () => {
+
+  return (
+    <>
+    {/* <Todo/> */}
+    {/* <PostForm/> */}
+    <Apifetch/>
+    {/* <ComponentA/> */}
+    </>
+  );
+};
+
+export default App;
